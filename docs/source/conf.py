9e140a482e0195ab2be03f4e10201615b8bce6ef
Kallio <liuke0617@gmail.com>
2026-01-27 14:38:45 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BIOmix'
+copyright = '2026, Kallio'
+author = 'Kallio'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0.1'
+version = '0.0.1'
 
 # -- General configuration
 
